ce609b92dc23640e05e5140035eebb42b9569d11
Crandel <cradlemann@gmail.com>
2017-09-22 18:03:59 +0300

    Add few tests and docs

--- src/accounts.rs
+++ src/accounts.rs
@@ -1,5 +1,3 @@
-use serde_derive;
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Account {
     account: String,
@@ -9,6 +7,7 @@ pub struct Account {
 }
 
 impl Account {
+    // Account constructor
     pub fn new(account: String, short: String, email: String, password: String,) -> Account {
         Account {
             account: account,
@@ -18,15 +17,39 @@ impl Account {
         }
     }
 
+    // public getter for email
     pub fn get_email(&self) -> &str {
         &self.email
     }
 
+    // public getter for password
     pub fn get_password(&self) -> &str {
         &self.password
     }
 
+    // public getter for short_conky value
     pub fn get_short(&self) -> &str {
         &self.short_conky
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Account;
+    #[test]
+    fn acc_test() {
+        let name = "test_name";
+        let email = "test_email";
+        let password = "test_password";
+        let short = "test_short";
+        let acc = Account::new(
+            String::from(name),
+            String::from(short),
+            String::from(email),
+            String::from(password),
+        );
+        assert_eq!(email, acc.get_email());
+        assert_eq!(password, acc.get_password());
+        assert_eq!(short, acc.get_short());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,16 @@ use regex::Regex;
 use gmail_lib::config;
 
 fn main(){
+    // result should be printed to stdout;
     let mut result_str = "".to_owned();
+    // config filename
     let config_file = ".gmail.json";
+    // gmail url
     let uri = "https://mail.google.com/mail/feed/atom";
     let uri = uri.parse::<Uri>().unwrap();
+    // get data from config file
     let data = config::get_config_data(config_file);
+    // extract accs info from Result
     let accs = match data {
         Ok(accs) => accs,
         Err(e) => {
@@ -32,16 +37,21 @@ fn main(){
             return
         },
     };
+    // creating http request
     let mut core = Core::new().unwrap();
     let handle = core.handle();
     let client = Client::configure()
         .connector(HttpsConnector::new(4, &handle).unwrap())
         .build(&handle);
+    // Regex part
     let fullcount = Regex::new("<fullcount>(.*?)</fullcount>").unwrap();
     let re = Regex::new("[0-9+]").unwrap();
+
+    // get number of unreaded messages for each acc
     for acc in &accs {
         let mut req = Request::new(Method::Get, uri.clone());
         {
+            // base64 header with sensitive data
             let mut headers = req.headers_mut();
             headers.set(Authorization(Basic {
                 username: String::from(acc.get_email()),
@@ -52,13 +62,16 @@ fn main(){
             res.body().concat2()
         });
         let result = core.run(gmail).unwrap();
+        // get info from response
         let body_str = match str::from_utf8(&result) {
             Ok(body) => body,
             _ => "",
         };
+        // extract necessary info using Regex
         let mat = fullcount.find(body_str).unwrap();
         let fullcount_str = &body_str[mat.start()..mat.end()];
         let result = re.find(fullcount_str).unwrap();
+        // Save result as String
         result_str.push_str(&format!("{}:{} ", acc.get_short(), &fullcount_str[result.start()..result.end()]));
     }
     println!("{}", result_str);
