b62153cf74fbd098289cc20914c79fb8aa3885ef
Crandel <cradlemann@gmail.com>
2020-02-08 22:40:12 +0100

    c

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use gmail_lib;
 use itertools::Itertools;
 use tokio::runtime::Runtime;
+use hyper::client::ResponseFuture;
+use futures::{
+    future::{
+        join_all,
+        JoinAll
+    },
+    Future,
+    Stream
+};
 
 use gmail_lib::{
     accounts::EmailType,
@@ -12,8 +21,6 @@ use gmail_lib::{
     gmail::GmailHandler,
     utils::ServiceUrl
 };
-use hyper::client::ResponseFuture;
-use futures::{Future, Stream};
 
 fn main() {
     // config filename
@@ -39,38 +46,40 @@ fn main() {
     let gmail_handler: GmailHandler = Default::default();
 
     // get number of unreaded messages for each acc
-    let account_futures: Vec<String> = accs.into_iter().map(
+    let account_futures: Vec<ResponseFuture> = accs.into_iter().map(
         |acc| {
             let handler = match acc.get_mail_type() {
                 EmailType::Gmail => &gmail_handler,
                 _ => &gmail_handler,
             };
+            web_client.send(handler.get_url(),
+                            acc.get_email(),
+                            acc.get_password())
+        }).collect();
 
-
-
-            let response = runtime.block_on(
-                web_client.send(handler.get_url(),
-                                acc.get_email(),
-                                acc.get_password())
-                    .map_err(WebClientError::HyperError)
-                    .and_then(|response| {
-                        let is_success = response.status().is_success();
-                        response.into_body().concat2().then(move |result| {
-                            let chunk = result.map_err(WebClientError::HyperError)?;
-                            if is_success {
-                                let bytes = chunk.into_bytes();
-                                let text: String = String::from_utf8_lossy(&bytes).into_owned();
-                                Ok(text)
-                            } else {
-                                let bytes = chunk.into_bytes();
-                                let text: String = String::from_utf8_lossy(&bytes).into_owned();
-                                Err(WebClientError::ConnectionError(text))
-                            }
-                        })
+    let join_futures: JoinAll<Vec<ResponseFuture>> = join_all(account_futures);
+    let responses = runtime.block_on(join_futures);
+    let account_strings: Vec<String> = responses.into_iter().map(
+        |resp| {
+            let response = resp.map_err(WebClientError::HyperError)
+                .and_then(|response| {
+                    let is_success = response.status().is_success();
+                    response.into_body().concat2().then(move |result| {
+                        let chunk = result.map_err(WebClientError::HyperError)?;
+                        if is_success {
+                            let bytes = chunk.into_bytes();
+                            let text: String = String::from_utf8_lossy(&bytes).into_owned();
+                            Ok(text)
+                        } else {
+                            let bytes = chunk.into_bytes();
+                            let text: String = String::from_utf8_lossy(&bytes).into_owned();
+                            Err(WebClientError::ConnectionError(text))
+                        }
                     })
-            );
+                });
             let body = match response {
-                Ok(bod) => handler.extract_result(bod),
+                // extract necessary info using Regex
+                Ok(bod) => gmail_handler.extract_result(bod),
                 Err(e) => match e {
                     WebClientError::HyperError(he) => {
                         eprintln!("{}", he);
@@ -82,9 +91,8 @@ fn main() {
                     }
                 }
             };
-            // extract necessary info using Regex
-            String::from(format!("{}:{}", acc.get_short(), body))
+            String::from(format!("{}:{}", "acc.get_short()", body))
         }).collect();
 
-    println!("{}", account_futures.iter().join(" "));
+    println!("{}", account_strings.iter().join(" "));
 }
